2a6df9cd005c7f2d05614ebd26f9313ce26f2794
Todd Wozny <toddwozny@gmail.com>
2016-02-11 22:11:19 +0000

    UserStory-4-WikiCrud complete

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -12,7 +12,7 @@ class WikisController < ApplicationController
   end
   
   def create
-    @wiki = wiki.new
+    @wiki = Wiki.new
     @wiki.title = params[:wiki][:title]
     @wiki.body = params[:wiki][:body]
 
@@ -27,5 +27,44 @@ class WikisController < ApplicationController
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+  
+   def update
+     @wiki = Wiki.find(params[:id])
+     @wiki.title = params[:wiki][:title]
+     @wiki.body = params[:wiki][:body]
+ 
+     if @wiki.save
+       flash[:notice] = "Wiki was updated."
+       redirect_to @wiki
+     else
+       flash.now[:alert] = "There was an error saving the wiki. Please try again."
+       render :edit
+     end
+   end
+   
+    def destroy
+     @wiki = Wiki.find(params[:id])
+ 
+ 
+     if @wiki.destroy
+       flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+       redirect_to wikis_path
+     else
+       flash.now[:alert] = "There was an error deleting the wiki."
+       render :show
+     end
+    end
+  
+  private 
+  
+  def wiki_params
+    #params.require(:wiki).permit()
+    
   end
 end
+
+
+#refactoring
+
